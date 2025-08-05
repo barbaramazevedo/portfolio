8dc45b32395239b90e54250dc21a93e540bb4f41
Bárbara Azevedo <barbara.azevedo22@gmail.com>
2025-08-05 19:02:35 -0300

    fix(nav-header): fix toggle menu responsiveness on small screens

--- src/controller/components/toggle-menu.js
+++ src/controller/components/toggle-menu.js
@@ -7,4 +7,24 @@ export function setupToggleMenu() {
     menuList.classList.toggle('active');
     actionsContent.classList.toggle('active');
   });
+
+  document.addEventListener('click', (e) => {
+    if (
+      menuList.classList.contains('active') &&
+      !menuList.contains(e.target) &&
+      !menuToggle.contains(e.target)
+    ) {
+      menuList.classList.remove('active');
+      actionsContent.classList.remove('active');
+    }
+  });
+
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' || e.key === 'Esc') {
+      if (menuList.classList.contains('active')) {
+        menuList.classList.remove('active');
+        actionsContent.classList.remove('active');
+      }
+    }
+  });
 }
